c5ce5407126ccd608929749079f1dd25cf546635
dnszhkv <den1szhuk0v@yandex.ru>
2023-12-04 14:40:07 +0700

    summary

--- pro/news/admin.py
+++ pro/news/admin.py
@@ -2,5 +2,19 @@ from django.contrib import admin
 from .models import Category, Post
 
 
+# Функция обнуления рейтинга постов
+def nullify_rating(modeladmin, request, queryset):
+    queryset.update(rating=0)
+nullify_rating.short_description = 'Обнулить рейтинги'
+
+
+# Создаю класс для представления постов в админке
+class PostAdmin(admin.ModelAdmin):
+    # список или кортеж со всеми полями, отображаемыми в таблице с постами
+    list_display = ('title', 'author', 'time_in', 'type', 'rating')
+    list_filter = ('author', 'rating', 'type')  # фильтры по полям
+    actions = [nullify_rating]  # добавляю функцию
+
+
 admin.site.register(Category)
-admin.site.register(Post)
+admin.site.register(Post, PostAdmin)
--- pro/pro/settings.py
+++ pro/pro/settings.py
@@ -148,7 +148,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
@@ -158,7 +158,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 
 
-DEBUG = False
+DEBUG = True
 EMAIL_USE_SSL = False
 EMAIL_USE_TLS = True
 EMAIL_HOST = config('EMAIL_HOST')
